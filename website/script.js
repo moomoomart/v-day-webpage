f1ed14892a2d741956152220387fc9ba5773892e
Valerie <157339846+moomoomart@users.noreply.github.com>
2024-01-22 12:41:45 +0800

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Yippee, see u on 14 Feb <3 Lub u hehe";
+    gif.src = 
+      "https://media.tenor.com/9L3dafxnOfoAAAAi/peach-goma-peach-and-goma.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+});
